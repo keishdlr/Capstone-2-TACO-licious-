bd33494da510c5827cb013f73baff12b07477a8e
keisha De La Rosa <keishdlr@gmail.com>
2025-11-10 14:30:21 -0500

    set up basic logic

--- src/main/java/models/Drink.java
+++ src/main/java/models/Drink.java
@@ -1,4 +1,20 @@
 package models;
 
-public class Drink {
+public class Chips implements Valuable {
+    private String name;
+    private double price;
+
+    public Chips(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    public String getSummary() {
+        return name + " - $" + String.format("%.2f", price);
+    }
 }
